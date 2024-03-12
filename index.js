851b4c809753441a14ecf647042999b40be5dd39
Roland-Sankara <roland1sankara@gmail.com>
2024-03-12 22:32:12 +0300

    update the project

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-import express from "express";
-import morgan from 'morgan';
-// import path from 'path'
-// import { fileURLToPath } from 'url';
+const express = require("express");
+const morgan  = require("morgan");
 
 const app = express()
 const PORT = process.env.PORT || 5000;
 // middleware 
 app.use(morgan('dev'))
 
-// const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-// const __dirname = path.dirname(__filename); // get the name of the directory
-
-// app.use(express.static(path.join(__dirname, "Public")))
 
 // Route Handler
 app.get('/', (req, res)=>{
