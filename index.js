468134565264fdb4a478d84b065ac6b2eed1c5a1
Roland-Sankara <roland1sankara@gmail.com>
2024-03-12 22:02:10 +0300

    feature: update the server

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 import express from "express";
 import morgan from 'morgan';
-import path from 'path'
-import { fileURLToPath } from 'url';
+// import path from 'path'
+// import { fileURLToPath } from 'url';
 
 const app = express()
-
+const PORT = process.env.PORT || 5000;
 // middleware 
 app.use(morgan('dev'))
 
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
+// const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+// const __dirname = path.dirname(__filename); // get the name of the directory
 
-app.use(express.static(path.join(__dirname, "Public")))
+// app.use(express.static(path.join(__dirname, "Public")))
 
 // Route Handler
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, "Public", "home.html"))
+    res.send("Welcome Home")
 })
 
 app.get('/contact', (req, res)=>{
-    res.sendFile(path.join(__dirname, "Public", "contact.html"))
+    res.send("Welcome to the Contact Page")
 })
 
-app.listen(5000, ()=>{
-    console.log('Server is listening on port 5000')
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`)
 })
