3165a873887d3391cd9660122dd8457b687abcb5
Roland-Sankara <roland1sankara@gmail.com>
2024-03-13 00:38:38 +0300

    update the project

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const express = require("express");
-const morgan  = require("morgan");
+import express from "express";
+import morgan  from "morgan";
+import path from "path";
+import {fileURLToPath} from "url";
 
 const app = express()
 const PORT = process.env.PORT || 5000;
-// middleware 
-app.use(morgan('dev'))
 
+// middleware 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(morgan('dev'))
+app.use(express.json(path.join(__dirname, "Public")));
 
 // Route Handler
 app.get('/', (req, res)=>{
-    res.send("Welcome Home")
+    res.sendFile(path.join(__dirname, "Public", "home.html"))
 })
 
 app.get('/contact', (req, res)=>{
-    res.send("Welcome to the Contact Page")
+    res.sendFile(path.join(__dirname, "Public", "contact.html"))
 })
 
 app.listen(PORT, ()=>{
